b2b4f97aff5a3dde43a62d25c82308071d7c34ad
DanielGlezExp <daniel.280596@gmail.com>
2020-03-12 18:59:09 +0000

    Conseguido obtener los nombres de los autores en un array

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -6,5 +6,7 @@ module.exports = rdf => {
   const book = {};
   const $ = cheerio.load(rdf);
   book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
+  book.title = $('dcterms\\:title').text();
+  book.authors = $('pgterms\\:agent pgterms\\:name').toArray().map(elem => $(elem).text());
   return book;
 };
